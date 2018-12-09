d4ae37590a908ecfc2813b25133ff08261db2375
thorstel <thorstel@pm.me>
2018-12-09 15:57:06 +0100

    (Day 8) Solve part 1 and 2 at the same time.

--- day08/day08.adb
+++ day08/day08.adb
@@ -2,42 +2,45 @@ with Ada.Containers.Vectors; use Ada.Containers;
 with Ada.Text_IO;            use Ada.Text_IO;
 
 procedure Day08 is
+   type Sum_Pair is record
+      Part1 : Natural;
+      Part2 : Natural;
+   end record;
+
    package Natural_Vectors is new Vectors
      (Index_Type   => Positive,
       Element_Type => Natural);
 
-   function Metadata_Sum
-     (Input    : in     Natural_Vectors.Vector;
-      Index    : in out Positive;
-      Is_Part2 : in     Boolean) return Natural
+   function Metadata_Sums
+     (Input : in     Natural_Vectors.Vector;
+      Index : in out Positive) return Sum_Pair
    is
-      Sum            : Natural := 0;
-      Child_Nodes    : Natural := Input (Index);
-      Metadata_Count : Natural := Input (Index + 1);
-      Child_Sums     : array (Positive range 1 .. Child_Nodes) of Natural :=
-        (others => 0);
+      Sums           : Sum_Pair := (0, 0);
+      Child_Nodes    : Natural  := Input (Index);
+      Metadata_Count : Natural  := Input (Index + 1);
+      Child_Sums     : array (Positive range 1 .. Child_Nodes) of Sum_Pair :=
+        (others => (0, 0));
    begin
       Index := Index + 2;
       for I in 1 .. Child_Nodes loop
-         Child_Sums (I) := Metadata_Sum (Input, Index, Is_Part2);
-         if not Is_Part2 then
-            Sum := Sum + Child_Sums (I);
-         end if;
+         Child_Sums (I) := Metadata_Sums (Input, Index);
+         Sums.Part1     := Sums.Part1 + Child_Sums (I).Part1;
       end loop;
       for I in 1 .. Metadata_Count loop
-         if Is_Part2 and Child_Nodes > 0 then
+         if Child_Nodes > 0 then
             if Input (Index) >= Child_Sums'First and
                Input (Index) <= Child_Sums'Last
             then
-               Sum := Sum + Child_Sums (Input (Index));
+               Sums.Part2 := Sums.Part2 + Child_Sums (Input (Index)).Part2;
             end if;
          else
-            Sum := Sum + Input (Index);
+            Sums.Part2 := Sums.Part2 + Input (Index);
          end if;
-         Index := Index + 1;
+         Sums.Part1 := Sums.Part1 + Input (Index);
+         Index      := Index + 1;
       end loop;
-      return Sum;
-   end Metadata_Sum;
+      return Sums;
+   end Metadata_Sums;
 
    File   : File_Type;
    Inputs : Natural_Vectors.Vector;
@@ -61,11 +64,9 @@ begin
    -- Part 1 and 2
    declare
       Index : Positive := Inputs.First_Index;
-      Sum   : Natural  := Metadata_Sum (Inputs, Index, False);
+      Sums  : Sum_Pair := Metadata_Sums (Inputs, Index);
    begin
-      Put_Line ("Part 1 =" & Natural'Image (Sum));
-      Index := Inputs.First_Index;
-      Sum   := Metadata_Sum (Inputs, Index, True);
-      Put_Line ("Part 2 =" & Natural'Image (Sum));
+      Put_Line ("Part 1 =" & Natural'Image (Sums.Part1));
+      Put_Line ("Part 2 =" & Natural'Image (Sums.Part2));
    end;
 end Day08;
